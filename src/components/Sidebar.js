f637cbe6df754e42f2421f380b54b49b02a13027
Ali Alkraida <alikraida1@gmail.com>
2025-04-19 17:21:38 +0300

    Add navbar and sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import "./Sidebar.css";
+import * as Icon from "react-bootstrap-icons";
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+
+      <Link to="/" className="item">
+        <Icon.House size={25} /> Home
+      </Link>
+
+      <Link to="/profile" className="item">
+        <Icon.Person size={25} /> Profile
+      </Link>
+
+      <Link to="/Settings" className="item">
+        <Icon.Gear size={25} /> Settings
+      </Link>
+
+    </div>
+  );
+}
+
+export default Sidebar;
