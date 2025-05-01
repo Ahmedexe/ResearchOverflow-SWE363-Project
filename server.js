0b715ce802ed1ec0ae90562ad4a13b68e3482056
Ahmedexe <alzuhairahmed@gmail.com>
2025-05-01 23:10:09 +0300

    initial Backend

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api', require('./routes/auth'));  // Mount the signup route
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
